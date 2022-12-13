9826fe6b85664a76b04a53bc1217573ce9dbc741
Gustavo Silva <gustavodev60@gmail.com>
2022-12-12 23:53:08 -0300

    feature: create teams entity

---
+++ lib/modules/teams/domain/entities/teams.dart
@@ -0,0 +1,16 @@
+class Teams {
+  final int id;
+  final String name;
+  final String nickName;
+  final String code;
+  final String city;
+  final String imageUrl;
+  Teams({
+    required this.id,
+    required this.name,
+    required this.nickName,
+    required this.code,
+    required this.city,
+    required this.imageUrl,
+  });
+}
