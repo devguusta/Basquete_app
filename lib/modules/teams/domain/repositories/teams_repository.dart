421835c7f64783b023fde7d7417072de44280d28
Gustavo Silva <gustavodev60@gmail.com>
2022-12-13 12:51:45 -0300

    clear project

--- lib/modules/teams/domain/repositories/teams_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:basquete_app/core/error/failure.dart';
-import 'package:basquete_app/modules/teams/domain/entities/teams.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class TeamsRepository {
-  Future<Either<Failure, List<Teams>>> fetchTeams();
-}
